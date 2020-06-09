2f608236d2dc5d675d2395867e45fc1b7a0b2b5e
kamillle <32205171+kamillle@users.noreply.github.com>
2020-06-10 02:17:42 +0900

    Initialize repo

---
+++ lib/active_record_string_encryption.rb
@@ -0,0 +1,6 @@
+require "active_record_string_encryption/version"
+
+module ActiveRecordStringEncryption
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/active_record_string_encryption/version.rb
@@ -0,0 +1,3 @@
+module ActiveRecordStringEncryption
+  VERSION = "0.1.0"
+end
